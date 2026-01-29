880d6cab7ba2d9e270d10211aac7906482bb0603
Michael Co <150204396+michael-j-co@users.noreply.github.com>
2026-01-29 09:57:12 -0800

    login bug fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,18 +5,13 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  // Start at the auth screen
-  initialRouteName: 'auth',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
